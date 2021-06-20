20f99a574967476078a4619c2c080c89451edc35
masato932 <masatowada66@gamil.com>
2021-06-20 09:58:19 +0900

    店舗リスト迄

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Store(models.Model):
+    name = models.CharField('店舗', max_length=100)
+    address = models.CharField('住所', max_length=100, null=True, blank=True)
+    tel = models.CharField('電話番号', max_length=100, null=True, blank=True)
+    description = models.TextField('説明', default="", blank=True)
+    image = models.ImageField(upload_to='images', verbose_name='イメージ画像', null=True, blank=True)
+
+    def __str__(self):
+        return self.name
---
+++ app/views.py
@@ -0,0 +1,12 @@
+from app.models import Store
+from django.views.generic import View
+from django.shortcuts import render
+
+
+class StoreView(View):
+    def get(self, request, *args, **kwargs):
+        store_data = Store.objects.all()
+
+        return render(request, 'app/store.html', {
+            'store_data': store_data,
+        })
